0099768190517494d2e0ab2c5cb522d65962a586
felipe_frosa <felipe.rosa@eldorado.org.br>
2022-08-24 17:01:50 -0300

    fix: cd class rebuild

--- controle-cd/projects/controle-cd-main/src/app/model/cd.ts
+++ controle-cd/projects/controle-cd-main/src/app/model/cd.ts
@@ -1,50 +1,20 @@
 export class Cd{
-  private _album: string;
-  private _artista: string;
-  private _ano: number;
-  private _estilo: string;
-  private _numFaixas: number;
+  public album: string;
+  public artista: string;
+  public ano: number;
+  public estilo: string;
+  public numFaixas: number;
 
   constructor (album: string,artista: string,ano: number,estilo: string,numFaixas: number){
-      this._album = album;
-      this._artista = artista;
-      this._ano = ano;
-      this._estilo = estilo;
-      this._numFaixas = numFaixas;
+      this.album = album;
+      this.artista = artista;
+      this.ano = ano;
+      this.estilo = estilo;
+      this.numFaixas = numFaixas;
     }
 
     public static clone(cd: Cd) {
       let c: Cd = new Cd(cd.album, cd.artista, cd.ano, cd.estilo, cd.numFaixas);
       return c;
     }
-  get album(){
-      return this._album;
-  }
-  set album(album){
-      this._album = album;
-  }
-  get artista(){
-      return this._artista;
-  }
-  set artista(artista){
-      this._artista = artista;
-  }
-  get ano(){
-      return this._ano;
-  }
-  set ano(ano){
-      this._ano = ano;
-  }
-  get estilo(){
-      return this._estilo;
-  }
-  set estilo(estilo){
-      this._estilo = estilo;
-  }
-  get numFaixas(){
-      return this._numFaixas;
-  }
-  set numFaixas(numFaixas){
-      this._numFaixas = numFaixas;
-  }
 }
